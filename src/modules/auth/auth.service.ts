8793db29cfa5725b51d20eeede1f2f12aaa9457c
ajiwijayatamaa <ajiwijayatamaa@gmail.com>
2026-02-07 22:05:19 +0700

    feat: add oauth google (#14)

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 import { PrismaClient, User } from "../../generated/prisma/client.js";
 import { comparePassword, hashPassword } from "../../lib/argon.js";
 import { ApiError } from "../../utils/api-error.js";
+import axios from "axios";
+import { UserInfo } from "../../types/google.js";
 
 interface RegisterBody extends Pick<User, "name" | "email" | "password"> {
   referrerCode?: string; // Kode referral milik orang lainn
@@ -98,6 +100,61 @@ export class AuthService {
     return {
       ...userWithoutPassword,
       accessToken: accessToken,
+    };
+  };
+
+  // GOOGLE
+  google = async (body: { accessToken: string }) => {
+    const { data } = await axios.get<UserInfo>(
+      "https://www.googleapis.com/oauth2/v3/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${body.accessToken}`,
+        },
+      },
+    );
+
+    const user = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+
+    // helper
+    const signToken = (user: { id: number; role: string }) =>
+      jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET!, {
+        expiresIn: "2h",
+      });
+
+    const sanitizeUser = <T extends { password?: string }>(user: T) => {
+      const { password, ...rest } = user;
+      return rest;
+    };
+
+    // user belum ada → create
+    if (!user) {
+      const newUser = await this.prisma.user.create({
+        data: {
+          name: data.name,
+          email: data.email,
+          password: "",
+          provider: "GOOGLE",
+        },
+      });
+
+      return {
+        ...sanitizeUser(newUser),
+        accessToken: signToken(newUser),
+      };
+    }
+
+    // user ada tapi bukan google
+    if (user.provider !== "GOOGLE") {
+      throw new ApiError("Account already registered without google", 400);
+    }
+
+    // user google existing
+    return {
+      ...sanitizeUser(user),
+      accessToken: signToken(user),
     };
   };
 }
