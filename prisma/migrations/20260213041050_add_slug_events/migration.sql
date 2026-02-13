9b7b09942e4c6feed7c0eed48e4409c4a16ccae0
ajiwijayatamaa <ajiwijayatamaa@gmail.com>
2026-02-14 00:32:53 +0700

    not finish

---
+++ prisma/migrations/20260213041050_add_slug_events/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `events` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[slug]` on the table `events` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `slug` to the `events` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "events" ADD COLUMN     "slug" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "events_name_key" ON "events"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "events_slug_key" ON "events"("slug");
